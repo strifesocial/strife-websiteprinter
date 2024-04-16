53f42ca119bae30ae835a0eabeb21333e875bc89
Strife Social <166969842+strifesocial@users.noreply.github.com>
2024-04-16 14:31:26 -0400

    Created - fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Strife Social'
+description 'A simple FiveM script to display a website link'
+version '1.0'
+
+lua54 'yes'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+
+-- Fair Usage Disclosure: 
+-- This file (fxmanifest.lua) and its accompanying scripts (client.lua, server.lua) are provided by Strife Social & Strife Social Developments under the Fair Usage policy. 
+-- You are allowed to reproduce, modify, and use this file and its accompanying scripts for the purpose of enhancing the functionality of your FiveM server, provided proper attribution is given to the original authors. 
+-- However, redistribution of this file and its accompanying scripts for commercial purposes without explicit permission from Strife Social & Strife Social Developments is prohibited.
